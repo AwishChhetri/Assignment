ba720233a6c8164eedab51d9027964cab3c797e3
AwishChhetri <95292407+AwishChhetri@users.noreply.github.com>
2023-07-03 12:51:55 +0530

    Styling user acc

--- Controller/account.js
+++ Controller/account.js
@@ -2,9 +2,10 @@ export const account=(req,res,next)=>{
     console.log("REQUIRE",req.user)
     if(req.user)
     {
-        res.render("account",{Photo:req.user.Image,Name:req.user.Name,
+        res.render("account",{Photo:req.user.Image,
+        Name:req.user.Name,
         Email:req.user.email,
-        Age:req.user.dob,
+        Id:req.user.dob,
     });
     }
     else
